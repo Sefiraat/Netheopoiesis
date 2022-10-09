e08e61b0569b97a1e10224c079cc6ead5ec375cf
Jeffrey Kosse <j3fftw@gmail.com>
2022-10-09 12:56:26 +0200

    fixes not full blocks being waterlogged

--- src/main/java/dev/sefiraat/netheopoiesis/listeners/WaterPlaceListener.java
+++ src/main/java/dev/sefiraat/netheopoiesis/listeners/WaterPlaceListener.java
@@ -4,6 +4,8 @@ import dev.sefiraat.netheopoiesis.Purification;
 import dev.sefiraat.netheopoiesis.utils.WorldUtils;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.Waterlogged;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -22,14 +24,25 @@ public class WaterPlaceListener implements Listener {
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onWaterPlace(@Nonnull PlayerInteractEvent event) {
         final Player player = event.getPlayer();
+        final Block clickedBlock = event.getClickedBlock();
         if (event.getClickedBlock() != null
             && event.getItem() != null
+            && clickedBlock != null
             && WorldUtils.inNether(player.getWorld())
             && player.getInventory().getItemInMainHand().getType() == Material.WATER_BUCKET
-            && Purification.getValue(event.getClickedBlock().getChunk()) >= Purification.PLACE_WATER
+            && Purification.getValue(clickedBlock.getChunk()) >= Purification.PLACE_WATER
         ) {
             event.setCancelled(true);
-            event.getClickedBlock().getRelative(event.getBlockFace()).setType(Material.WATER);
+
+            if (clickedBlock.getBlockData() instanceof Waterlogged block
+                && !block.isWaterlogged()
+                && !player.isSneaking()
+            ) {
+                block.setWaterlogged(true);
+            } else if (clickedBlock.getRelative(event.getBlockFace()).isEmpty()) {
+                clickedBlock.getRelative(event.getBlockFace()).setType(Material.WATER);
+            }
+
             if (player.getGameMode() != GameMode.CREATIVE) {
                 event.getItem().setType(Material.BUCKET);
             }
